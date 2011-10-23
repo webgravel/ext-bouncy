6e655ada5213ece330aa15a9f78967c9913ddbb6
James Halliday <mail@substack.net>
2011-10-23 15:46:33 -0700

    basic get test now working with parsley

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var ServerResponse = http.ServerResponse;
-var parsers = http.parsers;
+var parsley = require('parsley');
+var BufferedStream = require('morestreams').BufferedStream;
 
 var insertHeaders = require('./lib/insert_headers');
 var parseArgs = require('./lib/parse_args');
@@ -23,130 +24,60 @@ var bouncy = module.exports = function (opts, cb) {
 };
 
 var handler = bouncy.handler = function (cb, c) {
-    var parser = parsers.alloc();
-    parser.reinitialize('request');
-    parser.socket = c;
-    parser.incoming = null;
-    
-    var request = null;
-    parser.onIncoming = function (req, shouldKeepAlive) {
-        request = req;
-    };
-    
-    var buffers = [];
-    var bufSize = 0;
-    
-    function respond (req, bytesInHeader) {
-        var bufs = buffers;
-        buffers = [];
-        bufSize = 0;
+    parsley(c, function (req) {
+        var stream = new BufferedStream;
+        stream.pause();
         
-        req.socket.pause();
-        
-        var bounce = function (stream, opts) {
-            if (!stream || !stream.write) {
-                opts = parseArgs(arguments);
-                stream = opts.stream;
-            }
-            if (!opts) opts = {};
-            
-            if (!opts.hasOwnProperty('headers')) opts.headers = {};
-            
-            if (opts.headers) {
-                if (!opts.headers.hasOwnProperty('x-forwarded-for')) {
-                    opts.headers['x-forwarded-for'] = c.remoteAddress;
-                }
-                if (!opts.headers.hasOwnProperty('x-forwarded-port')) {
-                    var m = (req.headers.host || '').match(/:(\d+)/);
-                    opts.headers['x-forwarded-port'] = m && m[1] || 80;
-                }
-                if (!opts.headers.hasOwnProperty('x-forwarded-proto')) {
-                    opts.headers['x-forwarded-proto'] =
-                        c.encrypted ? 'https' : 'http';
-                }
-            }
-            
-            if (opts.headers) {
-                bytesInHeader += insertHeaders(bufs, opts.headers);
-            }
-            
-            var written = 0;
-            
-            for (var i = 0; i < bufs.length; i++) {
-                var buf = bufs[i];
-                
-                if (written + buf.length > bytesInHeader) {
-                    try {
-                        stream.write(buf.slice(0, bytesInHeader - written));
-                        break;
-                    }
-                    catch (err) {
-                        if (opts.emitter) {
-                            opts.emitter.emit('drop', c);
-                        }
-                        else {
-                            c.destroy();
-                        }
-                        return;
-                    }
-                }
-                else {
-                    try {
-                        stream.write(buf);
-                    }
-                    catch (err) {
-                        if (opts.emitter) {
-                            opts.emitter.emit('drop', c);
-                        }
-                        else {
-                            c.destroy();
-                        }
-                        return;
-                    }
-                    written += buf.length;
-                }
-            }
-            
-            req.socket.pipe(stream);
-            stream.pipe(c);
-            req.socket.resume();
-        };
-        
-        bounce.respond = function () {
-            var res = new ServerResponse(req);
-            res.assignSocket(req.socket);
-            return res;
-        };
-        
-        cb(req, bounce);
-    };
-    
-    c.on('data', function onData (buf) {
-        buffers.push(buf);
-        bufSize += buf.length;
-        
-        var ret = parser.execute(buf, 0, buf.length);
-        if (ret instanceof Error) {
-            c.destroy();
+        function onX (buf) {
+            stream.write(buf);
         }
-        else if (parser.incoming && parser.incoming.upgrade) {
-            c.removeListener('data', onData);
-            respond(parser.incoming, bufSize);
-            buffers = [];
-            bufSize = 0;
-        }
-        else if (request) {
-            respond(request, ret);
-            request = null;
-        }
-    });
-    
-    c.on('close', function () {
-        parsers.free(parser);
-    });
-    
-    c.on('end', function () {
-        parser.finish();
-        c.destroy();
+        req.on('rawHead', onX);
+        req.on('rawBody',  onX);
+        
+        req.on('end', function () {
+            req.removeListener('rawHead', onX);
+            req.removeListener('rawBody', onX);
+        });
+        
+        req.on('headers', function () {
+            var bounce = makeBounce(stream, c, req);
+            cb(req, bounce);
+        });
     });
 };
+
+function makeBounce (bs, client, req) {
+    var bounce = function (stream, opts) {
+        if (!stream || !stream.write) {
+            opts = parseArgs(arguments);
+            stream = opts.stream;
+        }
+        if (!opts) opts = {};
+        
+        if (!opts.headers) opts.headers = {};
+        if (!('x-forwarded-for' in opts.headers)) {
+            opts.headers['x-forwarded-for'] = client.remoteAddress;
+        }
+        if (!('x-forwarded-port' in opts.headers)) {
+            var m = (req.headers.host || '').match(/:(\d+)/);
+            opts.headers['x-forwarded-port'] = m && m[1] || 80;
+        }
+        if (!('x-forwarded-proto' in opts.headers)) {
+            opts.headers['x-forwarded-proto'] =
+                client.encrypted ? 'https' : 'http';
+        }
+        
+        insertHeaders(bs.chunks, opts.headers);
+        
+        if (stream.writable) bs.pipe(stream);
+        if (client.writable) stream.pipe(client);
+    };
+    
+    bounce.respond = function () {
+        var res = new ServerResponse(req);
+        res.assignSocket(client);
+        return res;
+    };
+    
+    return bounce;
+}
