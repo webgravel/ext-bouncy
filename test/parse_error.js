6fabccaba5a047bc08912bcd16f3294e3a7324fe
James Halliday <mail@substack.net>
2011-10-24 16:04:56 -0700

    failing parse error catching test

---
+++ test/parse_error.js
@@ -0,0 +1,25 @@
+var test = require('tap').test;
+var bouncy = require('bouncy');
+var net = require('net');
+
+test('parse error', function (t) {
+    var port = Math.floor(Math.random() * (Math.pow(2,16) - 1e4) + 1e4);
+    t.plan(2);
+    var sent = false;
+    
+    var s = bouncy(function (req, bounce) {
+        req.on('error', function (err) {
+            t.ok(err.message, 'error parsing method');
+            req.socket.end();
+            t.end();
+        });
+    });
+    
+    s.listen(port, function () {
+        var c = net.createConnection(port, function () {
+            c.write([
+                'rawr\r\n'
+            ].join('\r\n'));
+        });
+    });
+});
