f7df58d2a78c078944d0c62d22db59fab1446207
James Halliday <mail@substack.net>
2012-01-14 18:06:08 -0800

    update_url functionality with a failing test

---
+++ test/url.js
@@ -0,0 +1,26 @@
+var test = require('tap').test;
+var updateUrl = require('../lib/update_url');
+var chunky = require('chunky');
+
+test('update url', function (t) {
+    var times = 1;
+    t.plan(times * 2);
+    var msg = new Buffer([
+        'POST /beepity HTTP/1.1',
+        'Host: beep',
+        '',
+        'sound=boop'
+    ].join('\r\n'));
+    
+    for (var i = 0; i < times; i++) {
+        var chunks = chunky(msg);
+        
+        var n = updateUrl(chunks, '/boop');
+        t.equal(n, '/beepity'.length - '/boop'.length);
+        t.equal(
+            chunks.map(String).join(''),
+            msg.toString().replace(/\/beepity/, '/boop')
+        );
+    }
+    t.end();
+});
