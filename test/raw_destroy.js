dd409f52ce77214af01c7159821e868ee4575132
James Halliday <mail@substack.net>
2012-08-30 18:25:12 -0700

    fix raw destroy test to check try/catch for 0.6 and onerror for 0.8

--- test/raw_destroy.js
+++ test/raw_destroy.js
@@ -12,13 +12,13 @@ test('destroy the socket during piping', function (t) {
     var s0 = bouncy(function (req, bounce) {
         t.equal(req.headers.host, 'lulzy');
         
-        var c = net.createConnection(p1, function () {
+        var c = net.connect(p1, function () {
             bounce(c);
         });
     });
     
     s0.listen(p0, function () {
-        var c = net.createConnection(p0, function () {
+        var c = net.connect(p0, function () {
             c.write('POST /lul HTTP/1.1\r\n');
             c.write('Host: lulzy\r\n');
             c.write('Foo: bar\r\n');
@@ -26,14 +26,25 @@ test('destroy the socket during piping', function (t) {
             c.write('\r\n');
             
             setTimeout(function () {
-                t.throws(function () {
+                function onerror (err) {
+                    t.equal(
+                        err.message, 'This socket is closed.',
+                        'write to closed socket emits an error'
+                    );
+                }
+                
+                c.once('error', onerror); // node 0.8 emits an error
+                try {
                     c.write('a=3&b=4');
-                });
-                c.end();
-                s0.close();
-                s1.close();
-                t.end();
+                }
+                catch (err) {
+                    onerror(err); // node 0.6 throws
+                }
             }, 50);
+        });
+        
+        t.on('end', function () {
+            c.end();
         });
     });
     
@@ -43,4 +54,9 @@ test('destroy the socket during piping', function (t) {
         }, 20);
     });
     s1.listen(p1);
+    
+    t.on('end', function () {
+        s0.close();
+        s1.close();
+    });
 });
