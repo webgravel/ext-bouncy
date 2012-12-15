be7daa40cf8a668cd7a825dcdb2a079b45e403d0
James Halliday <mail@substack.net>
2012-12-14 21:10:29 -0800

    confirmed stable memory usage in the bench test, no need for test/leak.js

--- bench/bench.js
+++ bench/bench.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var name = process.argv[2];
 
-var p0 = Math.floor(Math.random() * (Math.pow(2,16) - 1e4)) + 1e4;
-var p1 = Math.floor(Math.random() * (Math.pow(2,16) - 1e4)) + 1e4;
+var p0 = 7000;
+var p1 = 7001;
 
 var proxy = require('./' + name)(p1);
 proxy.listen(p0);
@@ -13,4 +13,4 @@ var server = http.createServer(function (req, res) {
 });
 server.listen(p1);
 
-console.log('ab -n 100 -c 10 http://localhost:' + p0 + '/');
+console.log('ab -n 5000 -c 10 http://localhost:' + p0 + '/');
