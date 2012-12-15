397749e74093c11a2865aac240b34533b39471aa
James Halliday <mail@substack.net>
2012-12-15 13:41:15 -0800

    refactored into multiple bench tests

--- bench/bench.js
+++ bench/bench.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
-var name = process.argv[2];
-
+var http = require('http');
 var p0 = 7500;
 var p1 = 7501;
 
+var name = process.argv[3];
+var server = {
+    simple : http.createServer(function (req, res) {
+        res.end('beepity boop');
+    }),
+}[process.argv[2]];
+server.listen(p1);
+
 var proxy = require('./' + name)(p1);
 proxy.listen(p0);
-
-var http = require('http');
-var server = http.createServer(function (req, res) {
-    res.end('beepity boop');
-});
-server.listen(p1);
 
 console.log('ab -n 5000 -c 10 http://localhost:' + p0 + '/');
 
