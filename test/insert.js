91c6f7ac81f03cbde1992178a4ccc2c29c55ce8c
James Halliday <mail@substack.net>
2012-12-17 13:41:38 -0800

    all updated to use http-raw@1.1.0

--- test/insert.js
+++ test/insert.js
@@ -1,6 +1,6 @@
 var test = require('tap').test;
 var insertHeaders = require('../lib/insert_headers');
-var splitUp = require('./lib/split_up.js');
+var chunky = require('chunky');
 
 test('insert headers', function (t) {
     t.plan(50 * 3);
@@ -12,19 +12,19 @@ test('insert headers', function (t) {
     ].join('\r\n');
     
     for (var i = 0; i < 50; i++) {
-        var bufs = splitUp(msg);
-        t.equal(bufs.map(function (b) {
-            return String(b[0].slice(b[1],b[2]));
-        }).join(''), msg);
-        
-        var bufs_ = bufs.slice();
+        var bufs = chunky(msg);
+        t.equal(bufs.map(String).join(''), msg);
         
         var n = insertHeaders(bufs, { foo : 'bar', baz : 'quux' });
         t.equal(n, 'foo: bar\r\nbaz: quux\r\n'.length);
-        t.ok(
-            bufs.length === bufs_.length + 2
-            || bufs.length === bufs_.length + 3
-        );
+        t.equal(bufs.map(String).join(''), [
+            'POST / HTTP/1.1',
+            'Host: beep',
+            'foo: bar',
+            'baz: quux',
+            '',
+            'sound=boop'
+        ].join('\r\n'));
     }
     t.end();
 });
