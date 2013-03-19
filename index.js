80059a6b3bc3fd65fe45096a4510a27b93f65c83
James Halliday <mail@substack.net>
2013-03-19 00:39:17 -0700

    really basic http server approach

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var net = require('net');
+var http = require('http');
 var through = require('through');
 
 module.exports = function (cb) {
-    var server = net.createServer(function (stream) {
-        var src = through();
+    var server = http.createServer();
+    server.on('connection', function (stream) {
+        var src = stream._bouncySource = through();
         src.pause();
         stream.pipe(src);
-        
+    });
+    
+    server.on('request', function (req, res) {
+        var src = req.connection._bouncySource;
         var bounce = function (dst) {
-            src.pipe(dst).pipe(stream);
+            src.pipe(dst).pipe(req.connection);
             src.resume();
         };
-        cb({}, bounce);
+        
+        if (cb.length === 2) cb(req, bounce)
+        else cb(req, res, bounce)
     });
     return server;
 };
