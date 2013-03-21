97640a7192b25796a24bf805c280d2b47182fa74
James Halliday <mail@substack.net>
2013-03-21 01:47:13 -0700

    using parse_args, passing most of the bounce_url tests now

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var http = require('http');
 var through = require('through');
+var parseArgs = require('./lib/parse_args.js');
 
 module.exports = function (cb) {
     var server = http.createServer();
     server.on('connection', function (stream) {
-        var src = stream._bouncySource = through();
+        var src = stream._bouncyStream = through();
         src.pause();
         stream.pipe(src);
     });
     
     server.on('request', function (req, res) {
-        var src = req.connection._bouncySource;
+        var src = req.connection._bouncyStream;
         var bounce = function (dst) {
+            if (!dst || typeof dst.pipe !== 'function') {
+                dst = parseArgs(arguments);
+            }
+            
+            function destroy () {
+                src.destroy();
+                dst.destroy();
+            }
+            src.on('error', destroy);
+            dst.on('error', destroy);
+            
             src.pipe(dst).pipe(req.connection);
             src.resume();
         };
